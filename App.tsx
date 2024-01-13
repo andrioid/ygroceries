e714a9d1629db37d59c948430e011c1310efde25
Andri Óskarsson <account+github@andri.dk>
2024-01-13 10:30:11 +0100

    removed tailwind again, too messy

--- App.tsx
+++ App.tsx
@@ -1,9 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import { NativeWindStyleSheet } from "nativewind";
-NativeWindStyleSheet.setOutput({
-  default: "native",
-});
 
 export default function App() {
   return (
--- app/index.tsx
+++ app/index.tsx
@@ -46,9 +46,6 @@ export default function RootScreen() {
           />
         </Pressable>
       ))}
-      <View className="bg-red-500">
-        <Text className="text-2xl text-pink-600">tailwind test</Text>
-      </View>
     </View>
   );
 }
